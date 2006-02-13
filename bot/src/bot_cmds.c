c682b80fcbe67156057b6b395af92e3f757727a4
Gavin Hurlbut <gjhurlbu@gmail.com>
2006-02-13 23:40:51 +0000

    Enabled the svn:keyword Id Added bot_cmds.c to deal with commands the bot will respond to
    git-svn-id: https://trac.beirdo.ca:4443/svn/beirdobot/trunk@28 c66dcb5a-e80b-0410-ab0e-bb6a4f3ddef9

---
+++ bot/src/bot_cmds.c
@@ -0,0 +1,44 @@
+/*
+ *  This file is part of the beirdobot package
+ *  Copyright (C) 2006 Gavin Hurlbut
+ *
+ *  beirdobot is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*HEADER---------------------------------------------------
+* $Id$
+*
+* Copyright 2006 Gavin Hurlbut
+* All rights reserved
+*/
+
+/* INCLUDE FILES */
+#define ___ARGH
+#include "environment.h"
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* INTERNAL FUNCTION PROTOTYPES */
+
+/* CVS generated ID string */
+static char ident[] _UNUSED_ = 
+    "$Id$";
+
+
+/*
+ * vim:ts=4:sw=4:ai:et:si:sts=4
+ */
