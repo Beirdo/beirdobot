50250c42f987099e9153962bb94f24e4f7c85783
Gavin Hurlbut <gjhurlbu@gmail.com>
2006-06-23 21:14:11 +0000

    Updated the SQL schema at xris' request
    git-svn-id: https://trac.beirdo.ca:4443/svn/beirdobot/trunk@175 c66dcb5a-e80b-0410-ab0e-bb6a4f3ddef9

--- bot/docs/database.sql
+++ bot/docs/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 2.6.4-pl1-Debian-1ubuntu1.1
+-- version 2.6.4-pl1-Debian-1ubuntu1
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Feb 27, 2006 at 12:41 AM
+-- Generation Time: Jun 23, 2006 at 05:12 PM
 -- Server version: 4.0.24
--- PHP Version: 4.4.0-3ubuntu1
+-- PHP Version: 4.4.0-3
 -- 
 -- Database: `beirdobot`
 -- 
@@ -20,6 +20,7 @@ CREATE TABLE `channels` (
   `chanid` int(11) NOT NULL auto_increment,
   `serverid` int(11) NOT NULL default '0',
   `channel` varchar(64) NOT NULL default '',
+  `notify` int(11) NOT NULL default '0',
   `url` text NOT NULL,
   `notifywindow` int(11) NOT NULL default '24',
   `cmdChar` char(1) NOT NULL default '',
@@ -41,9 +42,25 @@ CREATE TABLE `irclog` (
   `msgtype` int(11) NOT NULL default '0',
   `message` text NOT NULL,
   PRIMARY KEY  (`msgid`),
-  KEY `timeChan` (`timestamp`,`chanid`),
-  KEY `messageType` (`chanid`,`msgtype`,`timestamp`),
+  KEY `timeChan` (`chanid`,`timestamp`),
+  KEY `messageType` (`msgtype`,`chanid`,`timestamp`),
+  KEY `chanid` (`chanid`),
+  KEY `timestamp` (`timestamp`),
   FULLTEXT KEY `searchtext` (`nick`,`message`)
+) TYPE=MyISAM;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `nickhistory`
+-- 
+
+CREATE TABLE `nickhistory` (
+  `chanid` int(11) NOT NULL default '0',
+  `nick` varchar(64) NOT NULL default '',
+  `histType` int(11) NOT NULL default '0',
+  `timestamp` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`chanid`,`histType`,`timestamp`,`nick`)
 ) TYPE=MyISAM;
 
 -- --------------------------------------------------------
@@ -59,6 +76,35 @@ CREATE TABLE `nicks` (
   `lastnotice` int(11) NOT NULL default '0',
   `present` int(11) NOT NULL default '0',
   PRIMARY KEY  (`chanid`,`nick`)
+) TYPE=MyISAM;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `plugin_rssfeed`
+-- 
+
+CREATE TABLE `plugin_rssfeed` (
+  `feedid` int(11) NOT NULL auto_increment,
+  `chanid` int(11) NOT NULL default '0',
+  `url` varchar(255) NOT NULL default '',
+  `prefix` varchar(64) NOT NULL default '',
+  `timeout` int(11) NOT NULL default '0',
+  `lastpost` int(11) NOT NULL default '0',
+  `feedoffset` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`feedid`)
+) TYPE=MyISAM;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `plugin_trac`
+-- 
+
+CREATE TABLE `plugin_trac` (
+  `chanid` int(11) NOT NULL default '0',
+  `url` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`chanid`)
 ) TYPE=MyISAM;
 
 -- --------------------------------------------------------
@@ -85,6 +131,7 @@ CREATE TABLE `servers` (
   `serverid` int(11) NOT NULL auto_increment,
   `server` varchar(255) NOT NULL default '',
   `port` int(11) NOT NULL default '0',
+  `password` varchar(255) NOT NULL default '',
   `nick` varchar(64) NOT NULL default '',
   `username` varchar(16) NOT NULL default '',
   `realname` varchar(255) NOT NULL default '',
@@ -104,4 +151,19 @@ CREATE TABLE `settings` (
   `name` varchar(80) NOT NULL default '',
   `value` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`name`)
+) TYPE=MyISAM;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `userauth`
+-- 
+
+CREATE TABLE `userauth` (
+  `username` varchar(64) NOT NULL default '',
+  `digest` enum('md4','md5','sha1') NOT NULL default 'md4',
+  `seed` varchar(20) NOT NULL default '',
+  `key` varchar(16) NOT NULL default '',
+  `keyIndex` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`username`)
 ) TYPE=MyISAM;
