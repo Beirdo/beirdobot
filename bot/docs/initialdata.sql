6b1ac7182c4eaca304d2c46bb9678ac23024822f
Gavin Hurlbut <gjhurlbu@gmail.com>
2006-07-11 20:49:55 +0000

    Updated the initial plugins data, to be used AFTER running the bot once
    git-svn-id: https://trac.beirdo.ca:4443/svn/beirdobot/trunk@196 c66dcb5a-e80b-0410-ab0e-bb6a4f3ddef9

--- bot/docs/initialdata.sql
+++ bot/docs/initialdata.sql
@@ -3,22 +3,21 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Feb 27, 2006 at 12:42 AM
+-- Generation Time: Jul 11, 2006 at 04:48 PM
 -- Server version: 4.0.24
--- PHP Version: 4.4.0-3ubuntu1
+-- PHP Version: 4.4.0-3ubuntu2
 -- 
 -- Database: `beirdobot`
 -- 
 
+-- --------------------------------------------------------
+
 -- 
--- Dumping data for table `plugins`
+-- Table structure for table `plugins`
 -- 
 
 INSERT INTO `plugins` VALUES ('trout', 'plugin_trout.so', 1, '');
 INSERT INTO `plugins` VALUES ('fart', 'plugin_fart.so', 1, '');
-
--- 
--- Dumping data for table `settings`
--- 
-
-INSERT INTO `settings` VALUES ('dbSchema', '1');
+INSERT INTO `plugins` VALUES ('core', 'plugin_core.so', 1, '');
+INSERT INTO `plugins` VALUES ('dns', 'plugin_dns.so', 1, '');
+INSERT INTO `plugins` VALUES ('rssfeed', 'plugin_rssfeed.so', 1, '');
